be37d5aee5221464264dd8cbed8ccd2acad47812
Bilal Khan <bilalkhan16496@gmail.com>
2025-08-18 19:32:20 +0500

    add task

---
+++ function.js
@@ -0,0 +1,142 @@
+ function addition (a,b) {
+    let sum = a+b
+
+    console.log(sum)
+}
+addition(1,2)
+
+
+ function additions (){
+    let n1 = 1
+    let n2 = 2
+    let n3 = n1+n2
+    console.log(n3)
+}
+additions()
+
+// subtraction
+ function subtra (a,b) {
+    let subi = a-b
+
+    console.log(subi)
+}
+ subtra(2,1)
+
+
+ function minuss (){
+    let n1 = 1
+    let n2 = 2
+    let n3 = n2-n1
+    console.log(n3)
+}
+minuss()
+
+// multiplication
+
+function multip (a,b) {
+    let muliti= a*b
+
+    console.log(muliti)
+}
+multip(1,2)
+
+
+ function malti (){
+    let n1 = 1
+    let n2 = 2
+    let n3 = n1*n2
+    console.log(n3)
+}
+malti()
+
+// division
+function divission (a,b) {
+    let divi = a / b
+
+    console.log(divi)
+}
+divission (4,2)
+
+
+ function divisin (){
+    let n1 = 4
+    let n2 = 2
+    let n3 = n1/n2
+    console.log(n3)
+}
+ divisin()
+
+
+
+
+// arrrow function
+// addition
+
+add = (x,y) => {
+    let sum = x+y;
+    console.log(sum)
+
+}
+add (1,2)
+
+add = () => {
+    let n1 = 1
+    let n2 = 2 
+    let n3 =  n1+ n2
+    console.log(n3)
+}
+add ()
+
+// subtraction
+
+
+sub = (x,y) => {
+    let subtraction = y-x;
+    console.log(subtraction)
+
+}
+sub (1,2)
+
+sub = () => {
+    let n1 = 1
+    let n2 = 2 
+    let n3 =  n2-n1
+    console.log(n3)
+}
+sub ()
+
+
+
+// multiplication
+mul = (x,y) => {
+    let multi = x*y;
+    console.log(multi)
+
+}
+mul(1,2)
+
+multiplication = () => {
+    let n1 = 1
+    let n2 = 2 
+    let n3 =  n1* n2
+    console.log(n3)
+}
+multiplication  ()
+
+
+// division
+
+div = (x,y) => {
+    let sum = x / y;
+    console.log(sum)
+
+}
+div (4,2)
+
+division = () => {
+    let n1 = 4
+    let n2 = 2 
+    let n3 =  n1 / n2
+    console.log(n3)
+}
+division ()
